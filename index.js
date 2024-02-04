6a567ec48af9b069966387636ebfc9984deba003
afsananur <afsanashahariar12@gmail.com>
2024-02-04 17:49:11 +0600

    final

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.POTT || 3000;
+const port = process.env.PORT || 3000;
 
 //midleware
 
